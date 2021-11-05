dbfdb714b6d7751fac0c4eb581adf2fef5b0ec5d
lamnaourm <73022871+lamnaourm@users.noreply.github.com>
2021-11-05 11:45:24 +0000

    Second commit

--- src/com/generic/exercice/main/Main.java
+++ src/com/generic/exercice/main/Main.java
@@ -15,19 +15,26 @@ public class Main {
 		System.out.print("Donner la date de naissance");
 		LocalDate date = LocalDate.parse(sc.nextLine());
 		
+		
+		// Liste des particuliers
 		Particulier p1 = new Particulier(cin,nom,date);
 	    Particulier p2 = new Particulier("QA1222","ZAID",LocalDate.of(1988, 6, 23));
 	    
+	    // Liste des entreprises
 	    Entreprise e1 = new Entreprise("CGI","Rabat",123323.5);
 	    Entreprise e2 = new Entreprise("ATOS","CASA",129999.5);
 	    
+	    // Liste des comptes bancaires
 	    CompteBancaire<Particulier> c1 = new CompteBancaire<>(1,1023.44,p1);
 	    CompteBancaire<Particulier> c2 = new CompteBancaire<>(1,19923.44,p2);
 	    CompteBancaire<Entreprise> c3 = new CompteBancaire<>(1,8787823.44,e1);
 	    CompteBancaire<Entreprise> c4 = new CompteBancaire<>(1,7878723.44,e2);
 	    CompteBancaire<String> c5 = new CompteBancaire<>(1,23.44,"AZIZ");
 	    
+	    // Transfert de 1000 de c3 vers c1
 	    CompteBancaire.transfere(c3, c1, 1000);
+	    
+	    // Affichage des comptes
 	    System.out.println(c1);
 	    System.out.println(c2);
 	    System.out.println(c3);
